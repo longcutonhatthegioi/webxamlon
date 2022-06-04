e29026a26166ba107549c363773a1b7c4d853e17
longcutonhatthegioi <106880237+longcutonhatthegioi@users.noreply.github.com>
2022-06-04 23:29:28 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Chào nha 😏😏😏",
+  text2: "Tòn có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Tòn gấc đẹp try có phải không nào ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là Tòn địp trai nhất thí giúi :))",
+  text5: "Cậu mơ à??   ?",
+  text6: "Địp ơiii là địp:))",
+  text7: "Tòn gấc chi là địp trai đúng ko ?:))",
+  text8: "Gửi cho Tòn:>",
+  text9: "Đinh Đức Toàn địp trai nhấc thế giới",
+  text10: "Tòn bít mà , cám ơn cậu nha :)))",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Địp trai quá chứ gì ngại ghê á, về fb đi cu =))",
+  text12: "Oge lun :))",
 };
 
 $(document).ready(function () {
